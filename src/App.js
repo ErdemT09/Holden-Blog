be108cd7d8e9cba344fe3f6f3b8cc2328268d7ea
ErdemT09 <erdemturkoglu40@gmail.com>
2021-09-26 14:40:49 +0300

    Create Holden Blog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
-import './App.css';
+import { Container, Navbar, Nav, NavDropdown, Image, Row, Col, Card, Button } from 'react-bootstrap';
+
+const Navig = <><Navbar bg="danger" variant="dark" fixed="top" expand="lg">
+  <Container>
+    <Navbar.Brand href="home">Holden's Ryefield</Navbar.Brand>
+    <Nav className="container-fluid">
+      <Nav.Link href="#home">Home</Nav.Link>
+    </Nav>
+  </Container>
+</Navbar>
+  <br />
+  <br />
+  <br /></>;
+
+
+const posts = <>
+<h2>My Posts</h2>
+<br></br>
+<Card style={{width:"500px"}}>
+  <Card.Header>12.24.2021</Card.Header>
+  <Card.Body>
+    <Card.Title>Phonies</Card.Title>
+    <Card.Text style={{wordBreak: 'break-word'}}> 
+    Phonies.
+    </Card.Text>
+      <Image src="https://i.pinimg.com/originals/24/d5/e7/24d5e7ba10d9a69461e4e4940b2b0565.jpg" style={{ width: "240px", height: "240px" }}></Image>
+  </Card.Body>
+</Card>
+</>;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Navig}
+      <Container className="d-flex justify-content-between">
+        <Row>
+          <Col>
+            <Image src="https://i.pinimg.com/originals/24/d5/e7/24d5e7ba10d9a69461e4e4940b2b0565.jpg" style={{ width: "240px", height: "240px" }}></Image>
+          </Col>
+          <Col>
+            {posts}
+          </Col>
+
+        </Row>
+
+      </Container>
     </div>
   );
 }
